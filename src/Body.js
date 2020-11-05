43817a99cc43f64b1585ade6f33cb6e463131815
Liz Eidelson <lizeidelson@Lizs-MacBook-Pro.local>
2020-11-04 23:53:22 -0500

    set up app.js, body, header, and musicVideo

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Body = () => {
+	return (
+		<div>
+			<h2>I am the App Body 💪 </h2>
+		</div>
+	)
+}
+
+export default Body
---
+++ src/MusicVideo.js
@@ -0,0 +1,14 @@
+import React from "react"
+import YouTube from "react-youtube"
+
+const MusicVideo = () => {
+	return (
+		<div>
+			<YouTube videoId='5HI_xFQWiYU' />
+			<YouTube videoId='iIpfWORQWhU' />
+			<YouTube videoId='yRYFKcMa_Ek' />
+		</div>
+	)
+}
+
+export default MusicVideo
