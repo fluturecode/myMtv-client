eebc31b88de13ec1285df5d19074a8b05cb1fb0c
Liz Eidelson <lizeidelson@Lizs-MacBook-Pro.local>
2020-11-07 18:25:37 -0500

    added logo image

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import logo from "./logo.jpeg"
+import mtv_logo from "./mtv_logo.jpg"
 import styled from "@emotion/styled"
 
 const Header = () => {
@@ -8,10 +8,17 @@ const Header = () => {
 		color: white;
 		padding: 5px;
 	`
+	// const Logo = styled.Logo`
+	// 	width: 80px;
+	// 	object-fit: contain;
+	// 	position: relative;
+	// 	border: 2px white;
+	// `
+
 	return (
 		<HeaderContainer>
 			<h1>I want MyMTV</h1>
-			{/* <link>{logo}</link> */}
+			<img src={mtv_logo} />
 		</HeaderContainer>
 	)
 }
