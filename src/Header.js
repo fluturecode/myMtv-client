3c05ba8e7edc7b975b4c45063c550b584cf5445e
fluturecode <fluturecode@gmail.com>
2020-11-15 01:49:24 -0500

    added build and firebase deploy

--- src/Header.js
+++ src/Header.js
@@ -13,28 +13,40 @@ const Header = () => {
 		align-items: center;
 		padding: 30px;
 		box-shadow: 0px 6px 18px -5px rgba(0, 0, 0, 0.75);
+		position: relative;
 	`
 
 	const HeaderText = styled.h1`
 		color: white;
 		font-style: italic;
+		padding-right: 10px;
 		&hover {
 		}
 	`
 
 	const LogoImage = styled.img`
-		height: 120px;
+		height: 140%;
 		object-fit: contain;
 		margin-right: 60px;
 		border-radius: 10px;
+		position: relative;
 	`
+	const AvatarImage = styled.div`
+		background-color: white;
+		width: 40px;
+		padding: 8px;
 
+		object-fit: contain;
+		border-radius: 100%;
+	`
 	return (
 		<>
 			<HeaderContainer>
 				<HeaderText>I Want My....</HeaderText>
 				<LogoImage src={mtv_logo} />
-				<Avatar />
+				<AvatarImage>
+					<Avatar src='https://avatars0.githubusercontent.com/u/59614789?s=460&u=59598c79c99b8067a17479b52a3281c239e0cdb7&v=4' />
+				</AvatarImage>
 			</HeaderContainer>
 		</>
 	)
