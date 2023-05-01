6b36a7eba6e984505e4d84b5e7458ac2304fdea3
Fluturecode <fluturecode@gmail.com>
2023-05-01 15:30:46 -0400

    updated footer text;

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ height: 8px;
 const Footer = () => {
   return (
     <Container>
-      <i>This is for demo purposes only, all rights reserved &copy; MTV.</i>
+      <i>For demo purposes only, all rights reserved &copy; 2023 MTV</i>
     </Container>
   )
 }
