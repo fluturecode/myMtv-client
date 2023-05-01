47d72e89edf442267cc63caf214461b0f8668baa
Fluturecode <fluturecode@gmail.com>
2023-05-01 15:38:13 -0400

    added coral cli

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Videos from "./Videos"
 import styled from "@emotion/styled"
+import { view, Text } from "react-xnft";
 
+const AppContainer = styled.div`
+background-color: grey;
+padding: 20px;
+height: 100%;
+`
 function App() {
-	const AppContainer = styled.div`
-		background-color: grey;
-		padding: 20px;
-		height: 100%;
-	`
 	return (
-		<AppContainer>
-			<Header />
-			<Videos />
-			<Footer />
-		</AppContainer>
+		<View>
+			<AppContainer>
+				<Header />
+				<Videos />
+				<Footer />
+			</AppContainer>
+		</View>
 	)
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import ReactXnft, { AnchorDom } from "react-xnft";
 
-ReactDOM.render(
-	<React.StrictMode>
+ReactXnft.render(
+	<AnchorDom>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</AnchorDom>
 )
 
 // If you want to start measuring performance in your app, pass a function
